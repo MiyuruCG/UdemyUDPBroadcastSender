f71a56d0a704378ab4be83a2e9494aa550872fe9
MiyuruCG <miyurugunatilake@gmail.com>
2020-03-29 12:37:28 +0530

    Edited this so the mesage that will be sent from the reciver can be received.

--- UdemyUDPBroadcastSender/Program.cs
+++ UdemyUDPBroadcastSender/Program.cs
@@ -31,8 +31,25 @@ namespace UdemyUDPBroadcastSender
 
             string strUserInput = string.Empty;
 
+
+            //need to create an endpoint object , so we will create IPendpoint object
+            IPEndPoint ipEpSender = new IPEndPoint(IPAddress.Any, 0);
+            //1 ::this endpoint will work with any network interface that is attached to this machine
+            //2 :: to use any freely available port no
+
+            //converting into a endpoint 
+            EndPoint epSender = (EndPoint)ipEpSender;
+
+            int countReceived = 0;
+            string txtReceived = string.Empty;
+
             try
             {
+                //we need to bind the socket in this application to receive data (to a endpoint)
+                sockBroadCaster.Bind(new IPEndPoint(IPAddress.Any, 0));
+                //para 1 :: is accecible through any network interface available on this machine
+                //para 2 :: the system will assign any available port no to this program
+
                 while (true)
                 {
 
@@ -50,6 +67,30 @@ namespace UdemyUDPBroadcastSender
                     sockBroadCaster.SendTo(broadcastBuffer, broadcastEP); //send operation
                     //1 :: the array we are sending 
                     //2 :: end point where the data we want to go
+
+                    if (strUserInput.Equals("<Echo>"))
+                    {
+                        /*
+                     //receive methord
+                     countReceived = sockBroadcastReceiver.Receive(receiverBuffer);
+                   */
+                        //change the receive methord to get the ip address of the sender 
+                        countReceived = sockBroadCaster.ReceiveFrom(broadcastBuffer, ref epSender);
+
+
+
+                        txtReceived = Encoding.ASCII.GetString(broadcastBuffer, 0, countReceived);
+                        //1 :: the received buffer
+                        //2 :: the starting point index of the getString operation
+                        //3 :: no of bytes we need to convert in to text
+
+                        Console.WriteLine("Number of byted recieved : " + countReceived);
+                        Console.WriteLine(" Received :: " + txtReceived);
+                        Console.WriteLine("Received from :: " + epSender.ToString());
+
+                       
+                    }
+
 
                 }//while ends here
 
